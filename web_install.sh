d26ca15381332faa96cb8b2ae9251e49e359e6e3
themadgnomeking <tims.m.guynes@gmail.com>
2019-09-14 16:20:46 -0700

    Initial Commit

---
+++ web_install.sh
@@ -0,0 +1,41 @@
+#!/usr/bin/env bash
+
+sudo apt update
+sudo apt install apache2
+
+sudo ufw allow in "Apache Full"
+
+sudo apt install mysql-server -y
+sudo mysql_secure_installation
+
+sudo apt install php libapache2-mod-php php-mysql -y
+
+location=/etc/apache2/mods-enabled/dir.conf
+
+cat > $location <<EOL
+<IfModule mod_dir.c>
+    DirectoryIndex index.php index.html index.cgi index.pl index.xhtml index.htm
+</IfModule>
+EOL
+
+sudo systemctl restart apache2
+
+sudo apt install php-cli
+
+#Virtual Hosting Setup
+#
+#your_domain=the domain name
+#
+#sudo mkdir /var/www/$your_domain
+#sudo chown -R $USER:$USER /var/www/$your_domain
+#sudo chmod -R 755 /var/www/$your_domain
+#
+#cat > /var/www/$your_domain/index.html <<EOL
+#<html>
+#    <head>
+#        <title>Welcome to $your_domain!</title>
+#    </head>
+#    <body>
+#        <h1>Success! The $your_domain server block is working!</h1>
+#    </body>
+#</html>
